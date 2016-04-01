10ec70f5a2b0567e37477f25ec97cbc7e03b0b6b
cassandrabarbe <cassandra.barbe@gmail.com>
2016-04-01 10:28:29 -0400

    idk what I'm doing help

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 var $body = $('body');
+
 for(i=0; i<75; i++) {
-  var $star = $('<div>');
-  $star.addClass('star');
-  $star.css({
-    'top': Math.random() * (document.documentElement.clientHeight + 300),
-    'left': Math.random() * (document.documentElement.clientHeight + 300),
-    'opacity': Math.random()
+    var $star = $('<div>');
+
+    $star.addClass('star');
+    $star.css({
+      'top': Math.random() * (document.documentElement.clientHeight + 300),
+      'left': Math.random() * (document.documentElement.clientHeight + 300),
+      'opacity': Math.random()
   });
   $body.append($star);
 }
