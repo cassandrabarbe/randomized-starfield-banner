762a3aef7c9fbb831c37469e2c224bf72f3a5da7
cassandrabarbe <cassandra.barbe@gmail.com>
2016-04-01 10:37:41 -0400

    indents

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ var $body = $('body');
 var $star;
 
 for (i = 0; i < 75; i++) {
-    $star = $('<div>');
-    $star.addClass('star');
-    $star.css({
-      'top': Math.random() * (document.documentElement.clientHeight + 300),
-      'left': Math.random() * (document.documentElement.clientHeight + 300),
-      'opacity': Math.random(),
-      'transform': Math.random()
+  $star = $('<div>');
+  $star.addClass('star');
+  $star.css({
+  'top': Math.random() * (document.documentElement.clientHeight + 300),
+  'left': Math.random() * (document.documentElement.clientHeight + 300),
+  'opacity': Math.random(),
+  'transform': Math.random()
   });
   $body.append($star);
 }
