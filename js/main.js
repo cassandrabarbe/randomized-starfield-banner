6451b07f0c6f9942970bd7efe66c18a7045f6283
cassandrabarbe <cassandra.barbe@gmail.com>
2016-04-01 10:35:08 -0400

    animations

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 var $body = $('body');
+var $star;
 
 for(i=0; i<75; i++) {
-    var $star = $('<div>');
-
+    $star = $('<div>')
     $star.addClass('star');
     $star.css({
       'top': Math.random() * (document.documentElement.clientHeight + 300),
       'left': Math.random() * (document.documentElement.clientHeight + 300),
-      'opacity': Math.random()
+      'opacity': Math.random(),
+      'transform': Math.random()
   });
   $body.append($star);
 }
