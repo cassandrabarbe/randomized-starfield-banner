936b71c1bc4064c706d54ae92bc4fd458f139bc3
cassandrabarbe <cassandra.barbe@gmail.com>
2016-04-01 10:23:34 -0400

    have stars... animating stars now

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var $body = $('body');
+for(i=0; i<75; i++) {
+  var $star = $('<div>');
+  $star.addClass('star');
+  $star.css({
+    'top': Math.random() * (document.documentElement.clientHeight + 300),
+    'left': Math.random() * (document.documentElement.clientHeight + 300),
+    'opacity': Math.random()
+  });
+  $body.append($star);
+}
