eef8e463210fed5366cccc3bf2d0c084e6a20a7d
cassandrabarbe <cassandra.barbe@gmail.com>
2016-04-01 10:38:35 -0400

    INDENTING

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ for (i = 0; i < 75; i++) {
   $star = $('<div>');
   $star.addClass('star');
   $star.css({
-  'top': Math.random() * (document.documentElement.clientHeight + 300),
-  'left': Math.random() * (document.documentElement.clientHeight + 300),
-  'opacity': Math.random(),
-  'transform': Math.random()
+    'top': Math.random() * (document.documentElement.clientHeight + 300),
+    'left': Math.random() * (document.documentElement.clientHeight + 300),
+    'opacity': Math.random(),
+    'transform': Math.random()
   });
   $body.append($star);
 }
